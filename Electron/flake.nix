0cd4e7b7b222e3f25fe2d7dd276b4d66adb945a0
biscotty666 <biscotty666@gmail.com>
2024-08-04 13:10:07 -0600

    Added electron flake;

---
+++ Electron/flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "A Nix-flake-based Node.js development environment";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+  };
+
+  outputs = { self , nixpkgs ,... }: let
+    # system should match the system you are running on
+    # system = "x86_64-linux";
+    system = "x86_64-linux";
+  in {
+    devShells."${system}".default = let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in pkgs.mkShell {
+      # create an environment with nodejs_18, pnpm, and yarn
+      packages = with pkgs; [
+        nodejs_22
+        nodePackages.pnpm
+        electron_29
+        electron_29-bin
+        dpkg
+        fakeroot
+        rpm
+      ];
+
+      shellHook = ''
+        echo "node `${pkgs.nodejs}/bin/node --version`"
+      '';
+    };
+  };
+}
