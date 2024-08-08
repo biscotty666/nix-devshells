7e0d111d72c32aedda20e57d409679dd618506dc
biscotty666 <biscotty666@gmail.com>
2024-08-07 21:48:25 -0600

    Modified Node flake;

--- Node/flake.nix
+++ Node/flake.nix
@@ -2,13 +2,13 @@
   description = "A Nix-flake-based Node.js development environment";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
   };
 
   outputs = { self , nixpkgs ,... }: let
     # system should match the system you are running on
     # system = "x86_64-linux";
-    system = "x86_64-darwin";
+    system = "x86_64-linux";
   in {
     devShells."${system}".default = let
       pkgs = import nixpkgs {
